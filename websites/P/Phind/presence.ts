eb5750dc16b55d881b2d85ed9df5d2febae03a78
GreenMan36 <Jaspervandenbremer@gmail.com>
2023-07-28 12:49:33 +0200

    feat(Phind): add support for agent mode

--- websites/P/Phind/presence.ts
+++ websites/P/Phind/presence.ts
@@ -2,7 +2,29 @@ const presence = new Presence({
 		clientId: "1106990410838065172",
 	}),
 	slideshow = presence.createSlideshow(),
-	browsingTimestamp = Math.floor(Date.now() / 1000);
+	browsingTimestamp = Math.floor(Date.now() / 1000),
+	getPathDetails = (
+		pathname: string
+	): { details: string; state: string } | null => {
+		const pathDetailsMap = {
+				"/filters": "Viewing Filters",
+				"/default": "Viewing how to set Phind as default",
+				"/bangs": "Viewing !Bangs",
+				"/mobile": "Viewing Mobile Page",
+				"/hotkeys": "Viewing Hotkeys",
+				"/history": "Viewing Search History",
+				"/about": "Viewing About Page",
+				"/tutorial": "Viewing Tutorial",
+				"/privacy": "Viewing Privacy Policy",
+				"/terms": "Viewing Terms",
+				"/extension/activate": "Activating IDE Extension",
+			},
+			pathDetails = Object.entries(pathDetailsMap).find(([pathPrefix]) =>
+				pathname.startsWith(pathPrefix)
+			);
+
+		return pathDetails ? { details: pathDetails[1], state: pathname } : null;
+	};
 
 presence.on("UpdateData", async () => {
 	const assets = {
@@ -13,22 +35,7 @@ presence.on("UpdateData", async () => {
 			largeImageKey: assets.PhindLogo,
 			details: "Browsing Phind",
 		},
-		pathDetailsMap = {
-			"/filters": "Viewing Filters",
-			"/default": "Viewing how to set Phind as default",
-			"/bangs": "Viewing !Bangs",
-			"/mobile": "Viewing Mobile Page",
-			"/hotkeys": "Viewing Hotkeys",
-			"/history": "Viewing Search History",
-			"/about": "Viewing About Page",
-			"/tutorial": "Viewing Tutorial",
-			"/privacy": "Viewing Privacy Policy",
-			"/terms": "Viewing Terms",
-		},
 		{ pathname, href } = document.location,
-		pathDetails = Object.entries(pathDetailsMap).find(([pathPrefix]) =>
-			pathname.startsWith(pathPrefix)
-		),
 		searchResults = document.querySelectorAll("div.row[name^='answer-']"),
 		[displayTime, displaySearch, cycleSearch, shareSearch, privateMode] =
 			await Promise.all([
@@ -37,19 +44,27 @@ presence.on("UpdateData", async () => {
 				presence.getSetting("cycleSearch"),
 				presence.getSetting("shareSearch"),
 				presence.getSetting("privateMode"),
-			]);
+			]),
+		pathDetails = getPathDetails(pathname);
 
 	if (displayTime) presenceData.startTimestamp = browsingTimestamp;
 
 	if (pathDetails) {
-		presenceData.details = pathDetails[1]; // display text of /path
-		presenceData.state = pathname; // display /path
-		slideshow.deleteAllSlides();
-	} else if (searchResults.length > 0 && !privateMode) {
-		// we must be on /search
-		presenceData.details = displaySearch ? "Searching for:" : "Searching";
+		presenceData.details = pathDetails.details;
+		presenceData.state = pathDetails.state;
+	} else if (
+		(searchResults.length > 0 && pathname === "/agent") ||
+		pathname === "/search"
+	) {
+		if (pathname === "/agent") {
+			presenceData.details = "Using agent:";
+			presenceData.smallImageText = "Using agent";
+		} else if (pathname === "/search") {
+			presenceData.details = "Searching for:";
+			presenceData.smallImageText = "Searching";
+		}
 		presenceData.smallImageKey = assets.Search;
-		presenceData.smallImageText = "Searching";
+
 		if (shareSearch) {
 			// cached result (exact same GPT output) : brand new search (different GPT output)
 			presenceData.buttons = [
@@ -65,12 +80,32 @@ presence.on("UpdateData", async () => {
 		if (displaySearch && cycleSearch && searchResults.length > 1) {
 			for (const [i, result] of searchResults.entries()) {
 				const newPresenceData: PresenceData = { ...presenceData };
-				newPresenceData.state = `${i + 1}. ${
-					result.querySelector("span.fw-bold.fs-3.mb-3").textContent
-				}`;
+
+				if (pathname === "/agent") {
+					console.log(result);
+					newPresenceData.details = `${i + 1}. ${[
+						...result.querySelectorAll("span.badge"),
+					]
+						.map(badge => badge.textContent)
+						.join(", ")}`;
+					newPresenceData.state = `${
+						result.querySelector("div.row div.card-body .text-black")
+							?.textContent
+					}`;
+				} else if (pathname === "/search") {
+					newPresenceData.details = "Searching for:";
+					newPresenceData.state = `${i + 1}. ${
+						result.querySelector("span.fw-bold.fs-3.mb-3").textContent
+					}`;
+				}
+
 				slideshow.addSlide(i.toString(), newPresenceData, 5000);
 			}
-		} else if (displaySearch) {
+		} else if (displaySearch && pathname === "/agent") {
+			presenceData.state = searchResults[0].querySelector(
+				"div.row div.card-body > div > div > p"
+			).textContent;
+		} else if (displaySearch && pathname === "/search") {
 			presenceData.state = searchResults[0].querySelector(
 				"span.fw-bold.fs-3.mb-3"
 			).textContent;
